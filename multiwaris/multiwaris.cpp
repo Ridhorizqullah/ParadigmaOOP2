bb03ea7bb83eae879f190ed1ccb19e4ad4792c1e
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-27 10:52:57 +0700

    multiwaris selesai

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -4,11 +4,48 @@ using namespace std;
 class orang {
 public:
 	int umur;
-	
+
 	orang(int pUmur) :
 		umur(pUmur)
 	{
-		cout < , "orang dibuat dengan umur " << umur << "\n" << endl;
+		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
 	}
 };
 
+class pekerja : public orang {
+public:
+
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat" << umur << "\n" << endl;
+	}
+};
+
+class pelajar : public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pelajar dibuat" << umur << "\n" << endl;
+	}
+};
+
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat \n" << endl;
+	}
+};
+
+int main() {
+	budi a(12);
+
+	return 0;
+
+}
