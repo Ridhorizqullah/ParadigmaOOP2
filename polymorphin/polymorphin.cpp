7a6762fd82365b7ae616cfde9e895cb2c1b16f59
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-27 10:08:53 +0700

    virtual void

--- polymorphin/polymorphin.cpp
+++ polymorphin/polymorphin.cpp
@@ -8,32 +8,3 @@ public:
 		cout<<"pesan dari seseorang"<<endl;
 		}*/
 };
-
-class joko :public seseorang {
-public: 
-	void pesan() {
-		cout << "pesan dari joko" << endl;
-	}
-};
-
-class lia :public seseorang {
-public:
-	void pesan() {
-		cout << "pesan dari lia" << endl;
-	}
-};
-
-int main() {
-	seseorang* obyek;
-	joko a;
-	lia b;
-
-	obyek = &a;
-	obyek->pesan();
-	obyek = &b;
-	obyek->pesan();
-	//a.seseorang::pesan();
-
-	return 0;
-
-}
